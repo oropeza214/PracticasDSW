1b41f8893283a6ae2ad3a781898a56cee5fafd5a
oropeza214 <brandon.oropeza@uabc.edu.mx>
2023-10-03 18:30:22 -0700

    En esta practica, vi como usar JS con HTML e hicimos ejercicios de JS usando var y alerts

---
+++ Practica 24/script.js
@@ -0,0 +1,41 @@
+// var numero1 = 2;
+// var numero2 = 3;
+// var resultado = numero1 + numero2;
+
+// alert(resultado);
+
+// var mensaje = 'Bienvenido a nuestro sitio web.';
+// var texto_1 = " texto comillas dobles,";
+// var texto_2 = ' texto comillas simples...';
+
+// alert (mensaje + texto_1 + texto_2);
+
+// var clienteRegistrado = false;
+// var clienteNoRegistrado = true;
+
+// alert(clienteRegistrado);
+
+// var array = ['Tijuana', 'Rosarito', 'Ensenada', 'Mexicali'];
+
+// alert(array);
+
+var numero1 = parseFloat(prompt("Ingrese el primer numero: "));
+var numero2 = parseFloat(prompt("Ingrese el segundo numero: "));
+var resultadoSuma = numero1 + numero2;
+
+alert("El resultado de la suma es: "+resultadoSuma);
+
+
+var resultadoResta = numero1 - numero2;
+
+alert("El resultado de la resta es: "+resultadoResta);
+
+
+var resultadoMultiplicacion = numero1 * numero2;
+
+alert("El resultado de la multiplicacion es: "+resultadoMultiplicacion);
+
+
+var resultadoDivision = numero1 / numero2;
+
+alert("El resultado de la division es: "+resultadoDivision);    
