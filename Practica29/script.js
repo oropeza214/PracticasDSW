0506e7ba8bdb2e85b95dc67d972e1d9a4628081f
oropeza214 <brandon.oropeza@uabc.edu.mx>
2023-11-13 20:00:55 -0800

    muestre el mensaje Hola siete veces, en el 4to hola debe mostrar el mensaje soy el 4to y después del 7mo // hola el mensaje “Acabamos de saludar

---
+++ Practica29/script.js
@@ -0,0 +1,11 @@
+// - Con un for muestre el mensaje “Hola” siete veces, en el 4to hola debe mostrar el mensaje “soy el 4to” y después del 7mo
+// hola el mensaje “Acabamos de saludar”.
+
+for (let i = 1; i <= 7; i++) {
+    console.log("Hola");
+    if (i === 4) {
+        console.log("soy el 4to");
+    }
+}
+
+console.log("Acabamos de saludar");
