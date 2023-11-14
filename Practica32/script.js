d3fb681a4cb8bdc9a6f14fa9e6657ce724664942
oropeza214 <brandon.oropeza@uabc.edu.mx>
2023-11-13 20:07:05 -0800

    Declaración de 2 variales con numeros, y funcion de suma y reste con valor de retorno, y imprime en alert el resultado

---
+++ Practica32/script.js
@@ -0,0 +1,38 @@
+// var numero1 = 2;
+// var numero2 = 10;
+
+// function suma_y_muestra() {
+//     resultado = numero1 + numero2;
+
+//     alert("El resultado es " + resultado);
+// }
+
+// suma_y_muestra();
+
+
+
+
+
+// var numero1 = 2;
+// var numero2 = 10;
+
+// function suma_y_muestra(primerNumero, segundoNumero) {
+//     var resultado = primerNumero + segundoNumero;
+
+//     alert("El resultado es " + resultado);
+// }
+
+// suma_y_muestra(numero1, numero2);
+
+
+
+
+
+var numero1 = 2;
+var numero2 = 10;
+
+function suma_y_muestra() {
+    return numero1 + numero2;
+}
+
+alert("El resultado es " + suma_y_muestra());
